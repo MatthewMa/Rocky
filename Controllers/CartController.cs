34e556c685db79288a9cca71d68c75d148129d38
MatthewMa <masihua721@gmail.com>
2020-10-30 10:16:19 -0600

    ROLE BASED management and add inquiry mail

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Rocky.Data;
 using Rocky.Models;
@@ -16,11 +20,15 @@ namespace Rocky.Controllers
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostsEnvironment;
+        private readonly IEmailSender _emailSender;
         [BindProperty]
         public ProductUserViewModel ProductUserVM { get; set; }
-        public CartController(ApplicationDbContext db)
+        public CartController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment, IEmailSender emailSender)
         {
             _db = db;
+            _webHostsEnvironment = webHostEnvironment;
+            _emailSender = emailSender;
         }
         public IActionResult Index()
         {
@@ -56,8 +64,29 @@ namespace Rocky.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [ActionName("Summary")]
-        public IActionResult SummaryPost(ProductUserViewModel productUserViewModel)
-        {          
+        public async Task<IActionResult> SummaryPostAsync(ProductUserViewModel productUserViewModel)
+        {
+            string pathToTemplate = _webHostsEnvironment.WebRootPath + Path.DirectorySeparatorChar.ToString()
+                + "templates" + Path.DirectorySeparatorChar.ToString() + "inquiry.html";
+            var subject = "New Inquiry";
+            string htmlBody = "";
+            // Read template file to htmlBody
+            using(var sr = System.IO.File.OpenText(pathToTemplate))
+            {
+                htmlBody = sr.ReadToEnd();
+            }
+            StringBuilder productListSB = new StringBuilder();
+            foreach (var product in productUserViewModel.ProductList)
+            {
+                productListSB.Append($" - Name: {product.Name} <span style='font-size: 14px;'> (ID: {product.Id})</span><br/>");
+            }
+            string messageBody = string.Format(htmlBody,
+                productUserViewModel.ApplicationUser.FullName,
+                productUserViewModel.ApplicationUser.Email,
+                productUserViewModel.ApplicationUser.PhoneNumber,
+                productListSB.ToString());
+            // Send email to user
+            await _emailSender.SendEmailAsync(WC.AdminEmail, subject, messageBody);
             return RedirectToAction(nameof(InquiryConfirmation));
         }
 
--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,7 @@ using Rocky.Models.ViewModels;
 
 namespace Rocky.Controllers
 {
+    [Authorize(Roles = WC.AdminRole)]
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _db;
